2e3a2f4f5128b75727c2f56f47075a870025b61f
jayakarthi <jayakarthi.d@gmail.com>
2017-06-18 10:34:26 +0000

    Create demo.tf

---
+++ demo.tf
@@ -0,0 +1,18 @@
+/* Setup our aws provider */
+provider "aws" {
+ access_key = "AKIAIIA6KWZUZY35OH5A"
+ secret_key = "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"
+ region = "us-west-1"
+}
+resource "aws_instance" "demo" {
+ ami = "ami-7a85a01a"
+ instance_type = "t2.micro"
+ security_groups = ["launch-wizard-1"]
+ key_name = "Final"
+  root_block_device = {
+  volume_size = 30
+ }
+ tags = {
+ Name = "demo"
+ }
+}
